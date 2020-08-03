39ecfe45970082057083f54609e885256472278d
Diego Ordóñez <diego.ordonez@gmail.com>
2020-08-03 14:05:47 -0500

    Update

---
+++ app/build/generated/source/buildConfig/debug/daoc/btserver/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package daoc.btserver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "daoc.btserver";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
